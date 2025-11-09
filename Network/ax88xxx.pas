f88fd38e66f8e91cfcd6c42b17e0cbbfac4cc79f
Ultibo <admin@ultibo.org>
2025-11-10 10:20:20 +1100

    Update AX88XXX driver with changes from RTL

--- Network/ax88xxx.pas
+++ Network/ax88xxx.pas
@@ -1,7 +1,7 @@
 {
 ASIX AX88XXX based USB 2.0 Ethernet Driver.
 
-Copyright (C) 2023 - SoftOz Pty Ltd.
+Copyright (C) 2025 - SoftOz Pty Ltd.
 
 Arch
 ====
@@ -1330,21 +1330,8 @@ begin
  Result:=ERROR_NOT_READY;
  if Network.NetworkState <> NETWORK_STATE_OPEN then Exit;
 
- {Acquire the Lock}
- if MutexLock(Network.Lock) = ERROR_SUCCESS then
-  begin
-   try
-    {Free Entry (Receive Buffer)}
-    Result:=BufferFree(Entry);
-   finally
-    {Release the Lock}
-    MutexUnlock(Network.Lock);
-   end;
-  end
- else
-  begin
-   Result:=ERROR_CAN_NOT_COMPLETE;
-  end;
+ {Free Entry (Receive Buffer)}
+ Result:=BufferFree(Entry);
 end;
 
 {==============================================================================}
@@ -1377,22 +1364,20 @@ begin
    {Acquire the Lock}
    if MutexLock(Network.Lock) = ERROR_SUCCESS then
     begin
-     try
-      {Remove Entry}
-      Entry:=Network.ReceiveQueue.Entries[Network.ReceiveQueue.Start];
+     {Remove Entry}
+     Entry:=Network.ReceiveQueue.Entries[Network.ReceiveQueue.Start];
 
-      {Update Start}
-      Network.ReceiveQueue.Start:=(Network.ReceiveQueue.Start + 1) mod PAX88XXXNetwork(Network).ReceiveEntryCount;
+     {Update Start}
+     Network.ReceiveQueue.Start:=(Network.ReceiveQueue.Start + 1) mod PAX88XXXNetwork(Network).ReceiveEntryCount;
 
-      {Update Count}
-      Dec(Network.ReceiveQueue.Count);
+     {Update Count}
+     Dec(Network.ReceiveQueue.Count);
 
-      {Return Result}
-      Result:=ERROR_SUCCESS;
-     finally
-      {Release the Lock}
-      MutexUnlock(Network.Lock);
-     end;
+     {Return Result}
+     Result:=ERROR_SUCCESS;
+
+     {Release the Lock}
+     MutexUnlock(Network.Lock);
     end
    else
     begin
@@ -1438,29 +1423,27 @@ begin
    {Acquire the Lock}
    if MutexLock(Network.Lock) = ERROR_SUCCESS then
     begin
-     try
-      {Check Empty}
-      Empty:=(Network.TransmitQueue.Count = 0);
+     {Check Empty}
+     Empty:=(Network.TransmitQueue.Count = 0);
 
-      {Add Entry}
-      Network.TransmitQueue.Entries[(Network.TransmitQueue.Start + Network.TransmitQueue.Count) mod PAX88XXXNetwork(Network).TransmitEntryCount]:=Entry;
+     {Add Entry}
+     Network.TransmitQueue.Entries[(Network.TransmitQueue.Start + Network.TransmitQueue.Count) mod PAX88XXXNetwork(Network).TransmitEntryCount]:=Entry;
 
-      {Update Count}
-      Inc(Network.TransmitQueue.Count);
+     {Update Count}
+     Inc(Network.TransmitQueue.Count);
 
-      {Check Empty}
-      if Empty then
-       begin
-        {Start Transmit}
-        AX88XXXTransmitStart(PAX88XXXNetwork(Network));
-       end;
+     {Check Empty}
+     if Empty then
+      begin
+       {Start Transmit}
+       AX88XXXTransmitStart(PAX88XXXNetwork(Network));
+      end;
 
-      {Return Result}
-      Result:=ERROR_SUCCESS;
-     finally
-      {Release the Lock}
-      MutexUnlock(Network.Lock);
-     end;
+     {Return Result}
+     Result:=ERROR_SUCCESS;
+
+     {Release the Lock}
+     MutexUnlock(Network.Lock);
     end
    else
     begin
